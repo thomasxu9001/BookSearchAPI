f44d6051616e459150a979ba7d1f187dcbcaafa5
ThomasZhang <thomas@fergusapp.com>
2019-10-20 22:12:04 +1300

    Add Book_model and unittest

---
+++ application/models/Book_model.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Class Add_on_model
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $author
+ * @property string $description
+ */
+class Book_model extends CI_Model
+{
+	private $table_name = 'book';
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+
+	/**
+	 * @param string $search
+	 * @param int $page
+	 * @param int $page_size
+	 * @return array
+	 */
+	public function get_list(string $search = NULL, int $page = 1, int $page_size = 5): array
+	{
+		return $this->db->select('id, title, author')
+			->like('title', $search)
+			->limit($page_size, ($page - 1) * $page_size)
+			->get($this->table_name)
+			->result();
+	}
+
+	/**
+	 * @param int $id
+	 * @return stdClass
+	 */
+	public function get_book_details(int $id) :stdClass
+	{
+		return $this->db->where('id', $id)->get($this->table_name)->row();
+	}
+}
---
+++ application/tests/Test_book_model.php
@@ -0,0 +1,51 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/*
+ * Typically, we should have a separate test database.
+ * */
+
+final class Test_book_model extends TestCase
+{
+	public $ci;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->ci = &get_instance();
+		$this->ci->load->model('Book_model');
+	}
+
+	public function test_get_list()
+	{
+		// Get default search result
+		$result = $this->ci->Book_model->get_list();
+		$this->assertEquals(5, count($result));
+		$this->assertEquals('A Technique for Producing Ideas', $result[4]->title);
+
+		// Now let's do some key word search
+		$key_result = $this->ci->Book_model->get_list('Technique');
+		$this->assertEquals(1, count($key_result));
+		$this->assertEquals('A Technique for Producing Ideas', $key_result[0]->title);
+
+		// Use another key word to do a search
+		$new_key_result = $this->ci->Book_model->get_list('art');
+		$this->assertEquals(2, count($new_key_result));
+		$this->assertEquals('The Art of Possibility', $new_key_result[0]->title);
+		$this->assertEquals('The Art of War', $new_key_result[1]->title);
+
+	}
+	
+	public function test_get_book_details()
+	{
+		$book = $this->ci->Book_model->get_book_details(7);
+
+		$this->assertEquals(7, $book->id);
+		$this->assertEquals('Anything You Want', $book->title);
+		$this->assertEquals('Derek Sivers', $book->author);
+		$this->assertEquals('The Book in Three Sentences: Too many people spend their life pursuing things that don’t actually make them happy. When you make a business, you get to make a little universe where you create all the laws. Never forget that absolutely everything you do is for your customers.',
+			$book->description);
+
+	}
+}
