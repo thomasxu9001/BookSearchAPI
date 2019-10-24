171b57d6b4b5670bc0f5863f6e54bc7bf11bfcac
ThomasZhang <thomas@fergusapp.com>
2019-10-24 21:26:01 +1300

    Add more demo data

--- application/migrations/002_add_book_data.php
+++ application/migrations/002_add_book_data.php
@@ -133,6 +133,96 @@ class Migration_Add_book_data extends CI_Migration
 				'author' => 'Kamal Ravikant',
 				'description' => 'The Book in Three Sentences: Everyone has a truth that they need to live and share. For the author, that truth was committing to the daily practice of repeating the phrase “I love myself.” When you love yourself, life loves you back.'
 			),
+			array(
+				'title' => 'Marcel Duchamp: The Afternoon Interviews',
+				'author' => 'Calvin Tomkins',
+				'description' => 'The Book in Three Sentences: This book is a collection of transcriptions from a series of interviews between writer Calvin Tomkins and artist Marcel Duchamp. Duchamp believed strongly in doing work that was free from tradition and starting with as much of a blank slate as possible. He was also quite playful, worked slowly, and saw laziness as a good thing.'
+			),
+			array(
+				'title' => 'Margin of Safety: Risk-Averse Value Investing Strategies for the Thoughtful Investor',
+				'author' => 'Seth Klarman',
+				'description' => 'The Book in Three Sentences: Avoiding loss should be the primary goal of every investor. The way to avoid loss is by investing with a significant margin of safety. A margin of safety is necessary because valuation is an imprecise art, the future is unpredictable, and investors are human and make mistakes.'
+			),
+			array(
+				'title' => 'Mastermind Dinners',
+				'author' => 'Jayson Gaignard',
+				'description' => 'The Book in Three Sentences: Hosting dinners with like-minded people is one of the most powerful way to build fantastic relationships in business and in life. You should think carefully about who you invite to these meals and look for uncommon commonalities that make it more likely the guests will resonate with one another. Be the gatekeeper of your network and assume responsibility for the people you surround yourself with in life.'
+			),
+			array(
+				'title' => 'Mastery',
+				'author' => 'George Leonard',
+				'description' => 'The Book in Three Sentences: The most successful path to mastering anything is to practice for the sake of the practice itself, not for the result. All significant learning is composed of brief spurts of progress followed by long periods of work where if feels as if you are stuck on a plateau. There are no experts–only learners.'
+			),
+			array(
+				'title' => 'The Most Important Thing Illuminated: Uncommon Sense for the Thoughtful Investor',
+				'author' => 'Howard Marks',
+				'description' => 'The Book in Three Sentences: You can’t do the same things others do and expect to outperform. The most dependable way to outperform the market is to buy something for less than its value. It is price, not quality that determines value: high-quality assets can be risky, and low-quality assets can be safe.'
+			),
+			array(
+				'title' => 'Not Fade Away',
+				'author' => 'Laurence Shames and Peter Barton',
+				'description' => 'The Book in Three Sentences: It is more important to live fully than to live in a straight line. The surest gauge of the impact a life makes is how many other lives it touches. Nothing in life looks the same once you truly understand that you are not exempt from death.'
+			),
+			array(
+				'title' => 'The Nurture Assumption',
+				'author' => 'Judith Rich Harris',
+				'description' => 'The Book in Three Sentences: The two classic drivers of human development are nature (genes) and nurture (environment). Many people mistakenly believe nurture only refers to how parents raise their children. Although children do learn things from their parents, they learn far more from their peers. The world that children share with their peer group is what shapes their behavior, modifies the characteristics they were born with, and determines the sort of people they will be when they grow up.'
+			),
+			array(
+				'title' => 'On the Move: A Life',
+				'author' => 'Oliver Sacks',
+				'description' => 'The Book in Three Sentences: Oliver Sacks was a brilliant physician and a fantastic writer. He lived a full life that included dealing with criticism over being gay, attending medical school at Oxford University, experimenting with heavy drug use, traveling the United States and Canada by motorcycle, suffering life-threatening injuries, squatting a California state record of 600 pounds, and being honored by the Queen of England for his many books and storied career as a physician. Sacks is a symbol of the importance of writing, the power of exploration and inquisitiveness, and the need for empathy.'
+			),
+			array(
+				'title' => 'On the Shortness of Life',
+				'author' => 'Seneca',
+				'description' => 'The Book in Three Sentences: We all fear death, but life is long if you know how to use it. Putting things off is the biggest waste of life: it snatches away each day as it comes, and denies us the present by promising the future: live immediately. In any situation in life you will find delights and relaxations and pleasures if you are prepared to make light of your troubles and not let them distress you.'
+			),
+			array(
+				'title' => 'The Practicing Mind',
+				'author' => 'Thomas M. Sterner',
+				'description' => 'The Book in Three Sentences: All of life is practice in one form or another. Actively practicing something is very different from passively learning. You will never reach a level of performance that feels complete, so learn to love the art of practicing your skill.'
+			),
+			array(
+				'title' => 'The Reason I Jump',
+				'author' => 'Naoki Higashida',
+				'description' => 'The Book in Three Sentences: This book is an autobiography written by a 13-year-old boy from Japan about what it is like to live with autism. The way autistic people view the world is very different than the way we may perceive them to view the world. This disconnect between how we view and treat people with autism and how they actually view the world makes living with autism even more difficult.'
+			),
+			array(
+				'title' => 'Resplendent Light',
+				'author' => 'Christopher Burkett',
+				'description' => 'The Book in Three Sentences: A book of 68 photos by master landscape photographer Christopher Burkett.'
+			),
+			array(
+				'title' => 'The Richest Man in Babylon',
+				'author' => 'George S. Clason',
+				'description' => 'The Book in Three Sentences: Save at least 10 percent of everything you earn and do not confuse your necessary expenses with your desires. Work hard to improve your skills and ensure a future income because wealth is the result of a reliable income stream. You cannot arrive at the fullest measure of success until you crush the spirit of procrastination within you.'
+			),
+			array(
+				'title' => 'Rules for a Knight',
+				'author' => 'Ethan Hawke',
+				'description' => 'The Book in Three Sentences: This book contains a number of “rules for a knight,” which are lessons on how to live better. For example, 1) never announce that you are a knight, simply behave as one and 2) the only intelligent response to the ongoing gift of life is gratitude, and 3) how a knight lives is what is important, not on which particular afternoon he was born or on which specific morning he might die. Along with many other insights.'
+			),
+			array(
+				'title' => 'Sapiens',
+				'author' => 'Yuval Noah Harari',
+				'description' => 'The Book in Three Sentences: Human history has been shaped by three major revolutions: the Cognitive Revolution (70,000 years ago), the Agricultural Revolution (10,000 years ago), and the Scientific Revolution (500 years ago). These revolutions have empowered humans to do something no other form of life has done, which is to create and connect around ideas that do not physically exist (think religion, capitalism, and politics). These shared “myths” have enabled humans to take over the globe and have put humankind on the verge of overcoming the forces of natural selection.'
+			),
+			array(
+				'title' => 'Slipstream Time Hacking',
+				'author' => 'Benjamin Hardy',
+				'description' => 'The Book in Three Sentences: What if we measured our lives based on “distance” traveled rather than time elapsed? If we measure life by distance rather than time, then it becomes very clear that you can hack time by figuring out how to jump further along the timeline of life. This enables you to live many lives in one lifetime. For example, someone who retires at age 30 will free up an extra 40+ years of life compared to their peers, which means they can live an entire second life that many people will never get to experience.'
+			),
+			array(
+				'title' => 'Strangers to Ourselves',
+				'author' => 'Timothy Wilson',
+				'description' => 'The Book in Three Sentences: We do not realize how much the non-conscious mind impacts our behavior and personality. In many cases, the non-conscious mind influences our behavior more than our conscious thoughts do and the two minds will often conflict with one another, which can make it difficult to keep our desires and our actions in alignment. The first step to bringing our non-conscious inclinations into alignment with our conscious desires is to act more like the person we want to be.'
+			),
+			array(
+				'title' => 'Stumbling on Happiness',
+				'author' => 'Dan Gilbert',
+				'description' => 'The Book in Three Sentences: What makes humans different from every other animal is that we think about the future. However, our brains fall victim to a wide range of biases that cause our predictions of the future (and our memories of the past) to be inaccurate. Because of these mental errors it is remarkably difficult to predict what will make us feel happy.'
+			),
 		);
 
 		$this->db->insert_batch('book', $data);
